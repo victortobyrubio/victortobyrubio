3b3832fd01e3e70221043c2a89ba403ba2a6733e
victortobyrubio <50858744+victortobyrubio@users.noreply.github.com>
2021-11-22 14:40:15 +0800

    Create testVariablesConcepts.java

---
+++ testVariablesConcepts.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+      
+      String string = new String("This is a string");
+      String easier = "Wow, that was easy!";//can be used instead of declaring new String()
+      
+      int x = 5; // declare a primitive int
+      Integer y = 5; // object (5 is autoboxed) class equivalent of int 
+      
+      System.out.println(string + easier);
+      System.out.println(x);
+    }
+}
+
+//primitive types - boolean, byte, char, short, int, long, float, double
+//objects - instance of a class(the data type)
+
+//statically typed - variables up front(before compiling)
+//dynamically typed - variables do not have types
+//data types -
+
+//literals - the value ( whats inside -> " " );
+
+//variable - stores value
+//expression - evaluates value
+//operator - work/combine on operands to produce a value( + - * / )
+//operand - thing operator works on
