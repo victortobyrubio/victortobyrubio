fcf9dcf96e1e458937b10d1eb51c6c8b71b610e5
victortobyrubio <50858744+victortobyrubio@users.noreply.github.com>
2021-11-23 14:11:19 +0800

    Update testIO.java

--- testIO.java
+++ testIO.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//import java.math.BigDecimal; <- used for bigdecimal
 
 public class MyClass {
     public static void main(String args[]) {
@@ -9,5 +10,18 @@ public class MyClass {
       String name = scanner.nextLine();
       
       System.out.println("Hello " + name);
+        
     }
 }
+    //other samples:
+      //String w = scan.nextLine(); // takes string type values
+      //System.out.println(w);
+      //int y = scan.nextInt(); // takes int type values
+      //System.out.println(y);
+      //int a = y + 10;
+      //System.out.println(a);
+      //BigDecimal money = scan.nextBigDecimal(); //<- takes precise decimal number
+      //boolean b = scan.nextBoolean(); // takes boolean values(true or false not 1 or 0)
+
+//casting values
+//int x = (int)scan.nextDouble(); <- allows input to have decimal but only displays int before decimal
