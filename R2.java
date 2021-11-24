aa6b1d20a17256b797d1146d47b6c7e9b31e103e
victortobyrubio <50858744+victortobyrubio@users.noreply.github.com>
2021-11-24 14:55:32 +0800

    Create R2.java

---
+++ R2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class R2 {
+    public static void main(String args[]) {
+      Scanner scan = new Scanner(System.in);
+      int r1 = scan.nextInt();
+      int s = scan.nextInt();
+      int r2 = (2*s)-r1;
+      System.out.println(r2);
+    }
+}
