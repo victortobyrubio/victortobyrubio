7c21d82c26a5b958f18600f62209c9343d108162
victortobyrubio <50858744+victortobyrubio@users.noreply.github.com>
2021-11-24 16:12:35 +0800

    Create Juxtaposition.java

---
+++ Juxtaposition.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Juxtaposition {
+    public static void main(String args[]) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+      int t = scan.nextInt();
+      int m = scan.nextInt();
+      
+      System.out.println(n * t * m);
+     
+    }
+}
