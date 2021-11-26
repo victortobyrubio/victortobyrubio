50aa04ad15af8862de67e7ee22e4338539dea985
victortobyrubio <50858744+victortobyrubio@users.noreply.github.com>
2021-11-26 14:31:57 +0800

    Create TriangleArea.java

---
+++ TriangleArea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {//method
+      Scanner scan = new Scanner(System.in);
+      int b = scan.nextInt();
+      int h = scan.nextInt();
+      
+      System.out.println(0.5*(b)*(h));
+    }
+}
+
